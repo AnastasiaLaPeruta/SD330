76aa2b0ffe347534461655f99e218c8d09bca88f
AnastasiaLaPeruta <89801518+AnastasiaLaPeruta@users.noreply.github.com>
2024-05-06 17:54:29 -0400

    Update parking.js

--- parking.js
+++ parking.js
@@ -5,6 +5,12 @@
 			await fetch("https://anastasialaperuta.github.io/SD330//parking.json");
 		const data = await response.json();
 		
+		data.parkingLots.forEach(lot => {
+            htmlString += `<p>${lot.name}</p>`;
+        });
+		
+		document.body.innerHTML += htmlString;
+		
 		htmlString += "<p> The parking lots are: " +
 			data['parkingLots']['name'];
 			
