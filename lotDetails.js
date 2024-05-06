ac28a66562db1c3c4d8717f743f435a2f60fbba5
AnastasiaLaPeruta <89801518+AnastasiaLaPeruta@users.noreply.github.com>
2024-05-06 18:57:10 -0400

    Add files via upload

---
+++ lotDetails.js
@@ -0,0 +1,16 @@
+async function getDetails() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const lotIndex = urlParams.get('lotIndex');
+
+    const response = await fetch("https://anastasialaperuta.github.io/SD330//parking.json");
+    const data = await response.json();
+    const lot = data.parkingLots[lotIndex];
+
+    let detailsHtml = `<h2>Details for ${lot.name}</h2><ul>`;
+    lot.spots.forEach(spot => {
+        detailsHtml += `<li>Type: ${spot.type}, Handicap: ${spot.isHandicap}, Available: ${spot.isAvailable}, Time Reserved: ${spot.reservedTime}, Time Filled: ${spot.filledTime}</li>`;
+    });
+    detailsHtml += "</ul>";
+
+    document.getElementById('details').innerHTML = detailsHtml;
+}
