72057162af95f861d9e3aec1787c5bbe38c6a786
AnastasiaLaPeruta <89801518+AnastasiaLaPeruta@users.noreply.github.com>
2024-05-06 17:31:03 -0400

    added parking.js

---
+++ parking.js
@@ -0,0 +1,10 @@
+
+	function loadJSON(){
+		const response = 
+			await fetch("https://anastasialaperuta.github.io/SD330//parking.json");
+		const data = await response.json();
+		
+		htmlString += "<p> The parking lots are: " +
+			data['parkingLots']['name'];
+			
+	}
