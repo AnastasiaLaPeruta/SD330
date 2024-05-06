38316924b8f995f4649ef9a34f77740bad2e7c04
AnastasiaLaPeruta <89801518+AnastasiaLaPeruta@users.noreply.github.com>
2024-05-06 18:06:15 -0400

    makes sure data comes before footer

--- parking.js
+++ parking.js
@@ -1,17 +1,12 @@
+async function loadJSON() {
+    var htmlString = "<p>The parking lots are:</p>";
+	const response = await fetch("https://anastasialaperuta.github.io/SD330//parking.json");
+    const data = await response.json();
 
-	async function loadJSON(){
-		var htmlString = "";
-		const response = 
-			await fetch("https://anastasialaperuta.github.io/SD330//parking.json");
-		const data = await response.json();
-		
-		data.parkingLots.forEach(lot => {
-            htmlString += `<p>${lot.name}</p>`;
-        });
-		
-		document.body.innerHTML += htmlString;
-		
-		htmlString += "<p> The parking lots are: " +
-			data['parkingLots']['name'];
-			
-	}
+    data.parkingLots.forEach(lot => {
+		htmlString += `<p>${lot.name}</p>`;
+    });
+
+    document.getElementById('parkingLotNames').innerHTML = htmlString;
+    }
+}
