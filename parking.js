ff7baba5939a90ade1f0decfa883fb2be65b2d3a
AnastasiaLaPeruta <89801518+AnastasiaLaPeruta@users.noreply.github.com>
2024-05-06 18:23:59 -0400

    added clickable links

--- parking.js
+++ parking.js
@@ -1,11 +1,12 @@
 async function loadJSON() {
-    var htmlString = "<p>The parking lots are:</p>";
-	const response = await fetch("https://anastasialaperuta.github.io/SD330//parking.json");
+    var htmlString = "<p>The parking lots are:</p><ul>";
+    const response = await fetch("https://anastasialaperuta.github.io/SD330//parking.json");
     const data = await response.json();
 
-    data.parkingLots.forEach(lot => {
-		htmlString += `<p>${lot.name}</p>`;
+    data.parkingLots.forEach((lot, index) => {
+            htmlString += `<li><a href="lotDetails.html?lotIndex=${index}">${lot.name}</a></li>`;
     });
 
+    htmlString += "</ul>";
     document.getElementById('parkingLotNames').innerHTML = htmlString;
 }
