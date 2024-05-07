a41d09e4bdc7420687c859370eca95b06c03f779
AnastasiaLaPeruta <89801518+AnastasiaLaPeruta@users.noreply.github.com>
2024-05-07 09:47:31 -0400

    got rid of list

--- parking.js
+++ parking.js
@@ -1,12 +1,11 @@
 async function loadJSON() {
-    var htmlString = "<p>The parking lots are:</p><ul>";
+    var htmlString = "<p>The parking lots are:</p>";
     const response = await fetch("https://anastasialaperuta.github.io/SD330//parking.json");
     const data = await response.json();
 
     data.parkingLots.forEach((lot, index) => {
-            htmlString += `<li><a href="lotDetails.html?lotIndex=${index}">${lot.name}</a></li>`;
+            htmlString += `<a href="lotDetails.html?lotIndex=${index}">${lot.name}</a><br>`;
     });
 
-    htmlString += "</ul>";
     document.getElementById('parkingLotNames').innerHTML = htmlString;
 }
