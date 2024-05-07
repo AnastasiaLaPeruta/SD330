e8a09f9958a468af97660ad137ca331f5dcccfb5
AnastasiaLaPeruta <89801518+AnastasiaLaPeruta@users.noreply.github.com>
2024-05-07 11:08:27 -0400

    fixed how map appears

--- lotDetails.js
+++ lotDetails.js
@@ -8,7 +8,7 @@ async function getDetails() {
     const lot = data.parkingLots[lotIndex];
 
     let detailsHtml = `<h2>Details for ${lot.name}</h2><ul>`;
-	detailsHtml += `<img style="width: 200px; height: 200px; float:left;" src="${lot.mapFilename}" id="map" />`;
+	detailsHtml += `<img style="width: 200px; height: 200px; float:center;" src="${lot.mapFilename}" id="map" /><br>`;
 	lot.spots.forEach(spot => {
 		detailsHtml += `<li>Type: ${spot.type}, Handicap: ${spot.isHandicap}, Available: ${spot.isAvailable}, Time Reserved: ${spot.reservedTime}, Time Filled: ${spot.filledTime}</li>`;
 	});
